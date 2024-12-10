eaa9c05fc4019c47ba8035947ab3b767cd1df944
Tracy Al khoury <alkhourytraicy@gmail.com>
2024-12-10 15:44:10 +0200

    Updates on Navbar and styles

--- src/webparts/faq/components/Navbar.tsx
+++ src/webparts/faq/components/Navbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { DefaultButton } from "@fluentui/react"; // Optional, for styling
 import { useNavigate, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "./Faq.module.scss";
 
 interface NavComponentProps {
   logoUrl: string; // URL for the logo
@@ -16,7 +17,9 @@ const Navbar: React.FC<NavComponentProps> = ({ logoUrl }) => {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav
+      className={`navbar navbar-expand-lg navbar-dark shadow-sm ${styles["navbar-custom"]}`}
+    >
       <div className="container-fluid">
         <Link className="navbar-brand" to="/home">
           <img
@@ -26,6 +29,24 @@ const Navbar: React.FC<NavComponentProps> = ({ logoUrl }) => {
             className="d-inline-block align-top"
           />
         </Link>
+
+        <div className="navbar-nav d-flex ms-4">
+          <Link
+            className="nav-link text-white mx-2"
+            to="/about"
+            style={linkStyle}
+          >
+            About
+          </Link>
+          <Link
+            className="nav-link text-white mx-2"
+            to="/contactus"
+            style={linkStyle}
+          >
+            Contact Us
+          </Link>
+        </div>
+
         <button
           className="navbar-toggler"
           type="button"
@@ -37,16 +58,34 @@ const Navbar: React.FC<NavComponentProps> = ({ logoUrl }) => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <DefaultButton text="Logout" onClick={handleLogout} />
+              <DefaultButton
+                text="Logout"
+                onClick={handleLogout}
+                className={styles.logoutButtonStyle}
+                styles={{
+                  root: { fontWeight: 700 },
+                  label: { fontWeight: 700 },
+                }}
+              />
             </li>
           </ul>
         </div>
       </div>
     </nav>
   );
+};
+
+// Link styling for "About" and "Contact Us"
+const linkStyle = {
+  fontSize: "1.1rem",
+  fontWeight: "500",
+  padding: "8px 12px",
+  borderRadius: "5px",
+  transition: "background-color 0.3s ease, color 0.3s ease",
 };
 
 export default Navbar;
